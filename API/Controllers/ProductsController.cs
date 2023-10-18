918a91130ccc01b46081649720d038e7f2f66d57
Oguz Akdeniz <oguz.akdenizz@windowslive.com>
2023-10-19 01:27:28 +0300

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using API.Data.DataContext;
+using API.Data.DbModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+        public ProductsController(StoreContext storeContext)
+        {
+                _storeContext = storeContext;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts() => _storeContext.Products.ToList();
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id) => _storeContext.Products.Find(id);
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,29 @@
+using API.Data.DataContext;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(option=>option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
